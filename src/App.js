3a7a9204a7f55434cd279ae3ba2de7f17a6075c3
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-08-21 19:12:43 -0500

    added event handlers as a stretch

--- src/App.js
+++ src/App.js
@@ -9,6 +9,19 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 const [homeCount, setHomeCount] = useState(0);
 const [awayCount, setAwayCount] = useState(0);
+
+const handleLionsTouchdown = touchdown => {
+  setHomeCount(touchdown + 7)
+}
+const handleLionsFieldGoal = fieldGoal => {
+  setHomeCount(fieldGoal + 3)
+}
+const handleTigersTouchdown = touchdown => {
+  setAwayCount(touchdown + 7)
+}
+const handleTigersFieldGoal = fieldGoal => {
+  setAwayCount(fieldGoal + 3)
+}
   return (
     <div className="container">
       <section className="scoreboard">
@@ -30,12 +43,12 @@ const [awayCount, setAwayCount] = useState(0);
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => setHomeCount(homeCount + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal"onClick={() => setHomeCount(homeCount + 3)}>Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => handleLionsTouchdown(homeCount)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal"onClick={() => handleLionsFieldGoal(homeCount)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown"onClick={() => setAwayCount(awayCount + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal"onClick={() => setAwayCount(awayCount + 3)}>Away Field Goal</button>
+          <button className="awayButtons__touchdown"onClick={() => handleTigersTouchdown(awayCount)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal"onClick={() => handleTigersFieldGoal(awayCount)}>Away Field Goal</button>
         </div>
       </section>
     </div>
